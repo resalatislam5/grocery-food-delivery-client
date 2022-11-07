b270c90672a8c166d61f7d2d2eb42b26e7c132b7
Resalat <resalatislam5@gmail.com>
2022-11-08 03:57:37 +0600

    login upgate and signup

--- src/pages/shared/Login/Login.js
+++ src/pages/shared/Login/Login.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { FaFacebook,FaGoogle } from "react-icons/fa";
 const Login = () => {
     return (
@@ -7,14 +6,14 @@ const Login = () => {
             <div className=' w-96 border mx-auto h-96 mt-[5%] p-5'>
                 <h2 className='text-center text-2xl font-bold mb-5'>Login</h2>
                 <form className='flex flex-col'>
-                    <input className='border p-3 my-5 rounded-lg' type="email" name="email" placeholder='Enter Your Email' id="" />
-                    <input className='border p-3 rounded-lg' type="password" name="password" placeholder='Enter Your Password' id="" />
+                    <input className='border p-3 my-5 rounded-lg' type="email" name="email" placeholder='Enter Your Email' id="" required/>
+                    <input className='border p-3 rounded-lg' type="password" name="password" placeholder='Enter Your Password' id="" required/>
                     <button className='underline text-end mb-5 text-[#F86061]'>Forgotten password?</button>
                     <input className='border p-3 btn' type="submit" value="Login" />
                 </form>
                 <div className='flex justify-center mt-5 gap-5 text-3xl text-[#f75353]'>
-                    <p className='cursor-pointer'><FaGoogle /></p>
-                    <p className='cursor-pointer'><FaFacebook /></p>
+                    <button className='cursor-pointer'><FaGoogle /></button>
+                    <button className='cursor-pointer'><FaFacebook /></button>
                 </div>
             </div>
         </div>
--- src/pages/shared/SignUp/SignUp.js
+++ src/pages/shared/SignUp/SignUp.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { FaFacebook,FaGoogle } from "react-icons/fa";
 
 const SignUp = () => {
     return (
-        <div>
-            signUp
+        <div className='min-h-[50vh] mb-[2%]'>
+            <div className=' w-96 border mx-auto h-[480px] mt-[3%] p-5'>
+                <h2 className='text-center text-2xl font-bold mb-5'>Sign Up</h2>
+                <form className='flex flex-col'>
+                    <input className='border p-3  rounded-lg' type="text" name="nam" placeholder='Enter Your Email' id="" required/>
+                    <input className='border p-3 my-5 rounded-lg' type="email" name="email" placeholder='Enter Your Email' id="" required/>
+                    <input className='border p-3 rounded-lg' type="password" name="password" placeholder='Enter Your Password' id="" required/>
+                    <input className='border p-3 rounded-lg my-5' type="password" name="Confirm" placeholder='Enter Your confirm Password' id="" required/>
+                    <input className='border p-3 btn' type="submit" value="Sign Up" />
+                </form>
+                <div className='flex justify-center mt-5 gap-5 text-3xl text-[#f75353]'>
+                    <button className='cursor-pointer'><FaGoogle /></button>
+                    <button className='cursor-pointer'><FaFacebook /></button>
+                </div>
+            </div>
         </div>
     );
 };
